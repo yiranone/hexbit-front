7450487769895958842a9521c4aeda8883f92c41
yiranone <yiranone@users.noreply.github.com>
2026-08-18 17:33:07 +0800

    fix: proxy local api requests in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:18080",
+        changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:18080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
